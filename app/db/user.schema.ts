d1e0fd7b9b033ed090932f52699547c4729857ef
Eric Echemane <eechemane29@gmail.com>
2022-10-02 16:22:17 +0800

    new schema

--- app/db/user.schema.ts
+++ app/db/user.schema.ts
@@ -14,10 +14,34 @@ const userSchema = new Schema({
         type: String,
         required: [true, "Image is required"],
     },
-    password: {
-        type: String,
-        required: [true, "password is required"],
+    yearLevel: {
+        type: Number,
+        required: [true, "Year level is required"],
+        min: [7, "Year level must be greater than 0"],
+        max: [12, "Year level must be less than 13"],
     },
+    height: {
+        type: Number,
+        required: [true, "Height is required"],
+        min: [0, "Height cannot be zero"],
+    },
+    weight: {
+        type: Number,
+        required: [true, "Weight is required"],
+        min: [0, "Weight cannot be zero"],
+    },
+    birthDate: {
+        type: String,
+        require: [true, "Birth date is required"],
+    },
+    bmiHistory: [{
+        date: String,
+        bmi: Number,
+    }],
+    bmrHistory: [{
+        date: String,
+        bmr: Number,
+    }],
 });
 
 export type IUser = InferSchemaType<typeof userSchema>;
